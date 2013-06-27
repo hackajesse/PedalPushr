7734ad74d8fd3b87c3ee609e6167026b03beaf0c
Jesse Biroscak <hackajesse@gmail.com>
2013-06-26 23:35:52 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PedalPushr::Application.config.secret_token = '14b3d05df3cb4d7646bcbec78a9d429b739630ada08d68dd51f6b13129e82741e181e4c692d92b550d38833b00d14815bc7b2d1cc29012680ab8d5773ac8a662'
