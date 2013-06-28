d3b6615fe0d4fa2e402b8e6792d0f10f2bcd195f
Jesse Biroscak <hackajesse@gmail.com>
2013-06-27 21:03:22 -0400

    Add Bootstrap, implement header & footer partials, nav & search bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 PedalPushr::Application.routes.draw do
+  get 'about' => 'pages#about'
+
+  root :to => 'Pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
