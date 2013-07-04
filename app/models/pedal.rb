4886622fb594b78ae43bb517bad0bca66f63b90c
Jesse Biroscak <hackajesse@gmail.com>
2013-07-04 12:00:00 -0400

    Add table to CRUD pedals with bootstrap prettiness

--- app/models/pedal.rb
+++ app/models/pedal.rb
@@ -1,3 +1,6 @@
 class Pedal < ActiveRecord::Base
   attr_accessible :price, :rating, :title
+
+# check out Rails validations for length, presence, etc.
+  validates :title, presence: :true, length: { maximum: 100}
 end
