3377f5ffa7709cffe33947c0ba3fd2b643a89c2d
Jesse Biroscak <hackajesse@gmail.com>
2013-07-08 14:16:19 -0400

    Reroute root to pedals#index

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ PedalPushr::Application.routes.draw do
 
   get 'about' => 'pages#about'
 
-  root :to => 'Pages#home'
+  root :to => 'pedals#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
